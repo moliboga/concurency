0e07fe77e5ce967e0050f8b0b38e2bbf81418076
Valery Stopen <stopen.lera@gmail.com>
2022-06-29 13:35:26 +0300

    Initial commit

---
+++ src/main/java/org/example/ReturnOneCallable.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.concurrent.*;
+
+public class ReturnOneCallable {
+
+    private static int currentX;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService service = null;
+        try {
+            service = Executors.newSingleThreadExecutor();
+
+            Future<Integer> result = service.submit(() -> {
+                int x = 0;
+                for (int i = 0; i < 10; i++){
+                    x = i;
+                    currentX = i;
+                    Thread.sleep(2000);
+                }
+                return x;
+            });
+            System.out.println(result.get(10, TimeUnit.SECONDS));
+        }
+        catch (TimeoutException e){
+            System.out.println(currentX);
+        }
+        finally{
+            if(service != null) service.shutdown();
+        }
+    }
+}
